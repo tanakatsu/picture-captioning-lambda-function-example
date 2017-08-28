53cdbaafd560ca8935686d6fa2b4490437d950fb
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2017-08-28 18:26:53 +0900

    Use LAMBDA-PROXY integration

--- handler.py
+++ handler.py
@@ -1,15 +1,31 @@
 import vision_client
 import translate_client
 import os
+import json
+
 
 def GetPictureCaption(event, context):
     print(event)
 
-    if 'url' in event:
-        url = event['url']
-    else:
-        url = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Ch_mtn_zoo_giraffes_2003.jpg/1200px-Ch_mtn_zoo_giraffes_2003.jpg"
+    try:
+        # url = event["url"]  # if you don't use the http event with the LAMBDA-PROXY integration
+        url = event["queryStringParameters"]["url"]
+    except:
+        url = None
+
+    # url = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Ch_mtn_zoo_giraffes_2003.jpg/1200px-Ch_mtn_zoo_giraffes_2003.jpg"
     print('url=', url)
+
+    if not url:
+        result = {
+            "url": url,
+            "event": event
+        }
+        response = {
+            "statusCode": 200,
+            "body": result
+        }
+        return response
 
     key = os.environ['MS_COGNITIVE_VISION_API_KEY']
 
@@ -27,9 +43,18 @@ def GetPictureCaption(event, context):
     result_text = result_text.encode('utf-8')
     print(result_text)
 
-    return {
+    result = {
         "url": url,
         "caption_en": caption,
         "caption_ja": result_text,
         "event": event
     }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(result)
+    }
+
+    return response
+
+    # return result  # if you don't use http event with the LAMBDA-PROXY integration
