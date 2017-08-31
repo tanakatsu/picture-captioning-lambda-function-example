b0d0fac1e12f39f74e7ce659dda84d77ff40f9c6
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2017-08-31 21:58:36 +0900

    Add clients for py3

---
+++ py3_api_clients/translate_client.py
@@ -0,0 +1,47 @@
+import requests
+import xml.etree.ElementTree as ET
+import os
+
+
+class TranslateClient():
+
+    def __init__(self, api_key=None):
+        self.key = api_key
+        self.token = None
+
+    def get_token(self):
+        token = requests.post(
+            'https://api.cognitive.microsoft.com/sts/v1.0/issueToken',
+            params={
+                'Subscription-Key': self.key
+            }
+        ).text
+        # print('token=', token)
+        return token
+
+    def translate(self, word):
+        if not self.token:
+            self.token = self.get_token()
+
+        response = requests.get(
+            'https://api.microsofttranslator.com/v2/http.svc/Translate',
+            params={
+                'appid': 'Bearer ' + self.token,
+                'to': 'ja',
+                'text': word,
+                'category': 'generalnn'
+            }
+        ).text
+
+        translated_word = ET.fromstring(response).text
+        return translated_word
+
+
+if __name__ == "__main__":
+    word = "a group of giraffe standing on top of a dirt field"
+    key = os.environ['MS_COGNITIVE_TRANSLATE_API_KEY']
+    client = TranslateClient(api_key=key)
+    result = client.translate(word)
+    print(word)
+    print('->')
+    print(result)
